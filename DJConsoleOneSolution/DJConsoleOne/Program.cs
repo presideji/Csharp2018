8d1aef185d8de44f05d05072dc9b12569ae61bb6
Deji Akin <presideji@yahoo.com>
2018-02-28 21:29:14 +0000

    2nd Code

---
+++ DJConsoleOneSolution/DJConsoleOne/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJConsoleOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+           // Console.ReadKey();
+
+            //this a single line comments
+            /*for multi lines
+             * test
+             * ggggg
+             * 
+             */
+
+            string greetings = "Good evening Class";
+            Console.WriteLine(greetings);
+            Console.WriteLine(greetings);
+            
+            //the below is interpolation
+            string firstname = "deji";
+            string lastname = "ayo";
+            string comment = "This is the man";
+            Console.WriteLine("My name is " + firstname + " " + lastname );
+            Console.WriteLine("My name is {0} {1}", firstname, lastname);
+            Console.WriteLine();
+            Console.WriteLine("My name is " + firstname + " " + lastname + "." + " " + comment);
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ DJConsoleOneSolution/DJConsoleOne/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DJConsoleOne")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DJConsoleOne")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("372d2dfd-03cb-4c52-903b-5de16963908c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
